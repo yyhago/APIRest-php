a10a2a6aa43f3fb1f5ca42b73d45805a9b9f1527
Yhago Felipe <yhago.felipe.teles@gmail.com>
2024-02-03 15:56:13 -0300

    Implementação BaseController e alteração no index

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . "/Config/config.php";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
+
 if((isset($uri[1]) && $uri[1] != 'api') || (isset($uri[2]) && $uri[2] != 'v1')){
   header("HTTP/1.1 404 Not Found");
   exit();
@@ -18,4 +19,3 @@ $user = new UserController();
 
 $methodName = $uri[4] . 'Action';
 $user->{$methodName}();
-?>
